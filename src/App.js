295eb131d43e5068f891df6ef79f3e1869d280bc
Thao Phuong <22521171@gm.uit.edu.vn>
2025-04-23 09:47:01 +0700

    LAB4

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NavigationBar from "./components/Navbar.js";
+import MoviesList from "./components/MoviesList.js";
+import Movie from "./components/Movie.js";
+import AddReview from "./components/AddReview.js";
+import Login from "./components/Login.js";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <NavigationBar /> 
+      <div className="container mt-4">
+        <Routes>
+          <Route path="/" element={<MoviesList />} />
+          <Route path="/movies" element={<MoviesList />} />
+          <Route path="/movies/:id" element={<Movie />} />
+          <Route path="/movies/:id/review" element={<AddReview />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
-
 export default App;
---
+++ src/components/MoviesList.js
@@ -0,0 +1,5 @@
+import React from "react";
+function MoviesList() {
+  return <div>Danh sách phim</div>;
+}
+export default MoviesList;
